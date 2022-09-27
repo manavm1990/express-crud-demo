c45629de856bc7193e86331c3bc3ffbbfcf8745b
Manav Misra <manavm@visionwebsoft.com>
2022-09-27 15:45:58 -0500

    feat: add 404 middleware

--- app/index.js
+++ app/index.js
@@ -11,6 +11,9 @@ app.get("/", (_, res) => {
 
 app.use(express.json());
 app.use("/api/items", itemsRouter);
+app.use((_, res, __) => {
+  res.status(404).send("Not Found");
+});
 
 app.listen(config.port, () => {
   console.info(`App listening at http://localhost:${config.port}`);
